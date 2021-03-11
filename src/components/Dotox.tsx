714e60c785cc7e518706ba204847bd2588337c2f
jedi <hmgarcia06@gmail.com>
2021-03-11 14:49:37 -0500

    Changed date since relapse

--- src/components/Dotox.tsx
+++ src/components/Dotox.tsx
@@ -9,7 +9,7 @@ import {
 import { calculateDays } from "services/detox";
 
 function Dotox() {
-  const pushupsDate = "2021-01-04";
+  const pushupsDate = "2021-01-20";
   
   const componentDidMount = () => {
     // getDotoxes();
